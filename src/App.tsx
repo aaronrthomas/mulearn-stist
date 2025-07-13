1276cadc107c38db75a392b3bd65b97e16f6adc0
Aaronrthomas <aaronrthomas88@gmail.com>
2025-07-13 22:38:02 +0530

    so many pages are made

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
             <Navbar />
             <Home />
             <About />
+            <br /><br />
             <Events />
             <Gallery />
             <Statistics />
